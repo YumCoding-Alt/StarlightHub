e342b0a8e37a0e247849bd256ad4070e16741c7d
YumCoding-Alt <bonguyen09052005@gmail.com>
2025-03-02 21:23:26 +0700

    Update skidlamcho.lua

--- skidlamcho.lua
+++ skidlamcho.lua
@@ -1,6 +1,6 @@
 local player = game.Players. LocalPlayer
-local banDuration = 1337
-local reason = "Đang Fix Lỗi Sẽ Mở Vào Lúc 20:30"
+local banDuration = 99999
+local reason = "Trì Hoãn Để Update UI Tầm 3-5 ngày hoặc hơn"
 local message = string.format("You were kicked from this experience: \nYou are banned for [%d hours]\nReason: %s\nFuture bans will be PERMANENT. Do not break the rules.",
 banDuration,
 reason
